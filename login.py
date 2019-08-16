8b872ecc255255544aaf8aa9fb5034200813d506
klmnblzs <40501930+klmnblzs@users.noreply.github.com>
2019-08-16 18:55:42 +0200

    ⚡ Created the login file

---
+++ login.py
@@ -0,0 +1,43 @@
+import collections
+import datetime
+import math
+import json
+import subprocess
+import asyncio
+import random
+import glob
+import gc
+import psutil
+import sys
+import re
+import traceback
+import argparse
+import os
+import logging
+import requests
+import logging.handlers
+import discord
+import aiohttp
+import discord
+import requests
+import io
+import re
+import urllib.parse
+from bs4 import BeautifulSoup
+from json import load, dump
+from datetime import timezone
+from collections import namedtuple
+from discord.ext import commands
+
+TOKEN_AUTH = "authentication token"
+
+bot = commands.Bot(command_prefix='prefix')
+client = discord.Client()
+
+@client.event
+async def on_ready():
+
+	print(client.guilds)
+	print('I am ready to serve, commander!')
+
+client.run(TOKEN_AUTH, bot=False)
