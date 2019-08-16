4fc1ff3bb98c9f9139ea2651757d86dd86e94fe6
limee <41487884+devlimee@users.noreply.github.com>
2019-08-16 20:05:30 +0200

    ⚡ Created the purge command

---
+++ js/purge.js
@@ -0,0 +1,20 @@
+module.exports.run = async (client, message, args) => {
+	
+    if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send(`:x: You don't have permission for that.`)
+    if (!message.guild.member(client.user).hasPermission("MANAGE_MESSAGES")) return message.channel.send(`:x: Error, i don't have **MANAGE_MESSAGES** permission!`);
+
+    if (!args[0]) return message.channel.send(`:thinking: How many messages would you like to delete?`);
+    if (args[0] < 1) return message.channel.send(`:x: Please enter a number greater than 1.`);
+    if (args[0] > 100) return message.channel.send(`:x: Please enter a number less than 100.`);
+    if (isNaN(args[0])) return message.channel.send(`:x: Please enter a correct number.`);
+
+    message.channel.bulkDelete(args[0]).then(() => {
+        message.channel.send(`:white_check_mark: Deleted **${args[0]}** messages!`).then(message => message.delete(2750));
+    }).catch().catch((e) => message.channel.send(`:x: You cannot delete a message older than 14 days.`));
+};
+
+module.exports.help = {
+    name: "purge",
+    category: "moderation",
+    description: "clears the specified amount"
+}
